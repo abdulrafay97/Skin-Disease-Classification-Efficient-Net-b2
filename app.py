b506576facf995b4af5cfa01090cffd3c49e3ccf
Abdul Rafay <80894764+abdulrafay97@users.noreply.github.com>
2022-10-12 13:45:04 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+#Import Packages
+import numpy as np
+from PIL import Image
+import streamlit as st
+import warnings
+from io import BytesIO
+import base64
+import requests
+warnings.filterwarnings('ignore')
+
+icon = Image.open('skin.jpg')
+st.set_page_config(page_title='Classifier', page_icon = icon)
+st.header('Skin Disease Classifier')
+
+#Get Image
+file_up = st.file_uploader('Upload an Image', type = "jpg")
+
+def img_to_bytes(img):
+    buffered = BytesIO()
+    img.save(buffered, format=img.format)
+    img_str = base64.b64encode(buffered.getvalue())
+    strIMG = img_str.decode("utf-8")
+    resp = get_response(strIMG)
+    return resp
+
+def get_response(ImgStr):
+    url = 'https://0d9fc9cc-ecb4-4528-9b1e-ac453bc20061.syndic.ai'
+    data = {'url': ImgStr}
+
+    x = requests.post(url, data)
+    return x.text
+
+if file_up is not None:
+    # display image that user uploaded
+    image = Image.open(file_up)
+    result = img_to_bytes(image)
+    st.image(image, caption = 'Uploaded Image.', use_column_width = True)
+    st.write("Predictions: ",result)
